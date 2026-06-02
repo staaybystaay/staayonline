dddd55a3838154c9c5b2cad841af9e39c1749149
ellise_grant <101023936+ellisegrant@users.noreply.github.com>
2026-06-02 11:18:01 +0000

    Update BrandCollections.jsx

--- src/components/home/BrandCollections.jsx
+++ src/components/home/BrandCollections.jsx
@@ -11,7 +11,7 @@ const BR  = '#E4E0D8'
 const F   = { fontFamily: "'Inter', sans-serif" }
 
 const collections = [
-  { id: 'eden', image: '/Solenne.jpeg', label: 'SS 2026', name: 'Eden Collection',  sub: '10 pieces',  href: '/shop' },
+  { id: 'eden', image: '/Solenne-new.jpg', label: 'SS 2026', name: 'Eden Collection',  sub: '10 pieces',  href: '/shop' },
   { id: 'love', image: '/Ayla.jpeg',   label: 'SS 2026', name: 'The Love Edit',    sub: 'New season', href: '/shop' },
   { id: 'bold', image: '/Mira.jpeg',   label: 'SS 2026', name: 'Bold & Beautiful', sub: 'New season', href: '/shop' },
 ]
