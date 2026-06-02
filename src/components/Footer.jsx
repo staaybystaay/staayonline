2bca7fea748fc316b64a487d8ff5a63fcf2b0209
ellise_grant <101023936+ellisegrant@users.noreply.github.com>
2026-06-02 12:43:58 +0000

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { motion } from 'framer-motion'
 import { Link } from 'react-router-dom'
-import useCartStore from '../../store/useCartStore'
+
 
 const G   = '#B8903A'
 const GL  = '#F5ECD8'
