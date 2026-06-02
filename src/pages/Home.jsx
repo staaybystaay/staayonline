8121b94af0688b46d6bfcdb65fc270ede54240c9
ellise_grant <101023936+ellisegrant@users.noreply.github.com>
2026-06-02 14:24:13 +0000

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,17 @@
-import PromoPopup       from '../components/home/PromoPopup'
-import Hero             from '../components/home/Hero'
-import FeaturedProducts from '../components/home/FeaturedProducts'
-import Features         from '../components/home/Features'
-import BrandBar         from '../components/home/BrandBar'
-import Mission          from '../components/home/Mission'
-import LatestProducts   from '../components/home/LatestProducts'
+import PromoPopup    from '../components/home/PromoPopup'
+import Hero          from '../components/home/Hero'
+import NewIn         from '../components/home/NewIn'
+import Collections   from '../components/home/Collections'
+import Mission       from '../components/home/Mission'
+import LatestProducts from '../components/home/LatestProducts'
 
 export default function Home() {
   return (
     <main>
       <PromoPopup />
       <Hero />
-      <FeaturedProducts />
-      <Features />
-      <BrandBar />
+      <NewIn />
+      <Collections />
       <Mission />
       <LatestProducts />
     </main>
