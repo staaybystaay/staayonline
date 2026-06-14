04a14aa8c63dbb82570fd6b6b217fd0c97ff338a
Staay <staayonline@gmail.com>
2026-06-14 22:03:50 +0000

    Update Hero.jsx

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { motion, AnimatePresence } from 'framer-motion'
-import { imgHero } from '../lib/images'
 
 const G  = '#B8903A'
 const W  = '#FFFFFF'
