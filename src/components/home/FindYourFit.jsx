d16132f7a652cae1469e99bd34d228e7a96dddbd
Staay <staayonline@gmail.com>
2026-07-14 13:16:10 +0100

    Update FindYourFit.jsx

--- src/components/home/FindYourFit.jsx
+++ src/components/home/FindYourFit.jsx
@@ -6,9 +6,9 @@ const DK = '#1A1612'
 const F  = { fontFamily: "'Inter', sans-serif" }
 
 const categories = [
-  { label: 'Eden Collection',  path: '/shop?col=eden',      image: './collection1.jpg' },
+  { label: 'Eden Collection',  path: '/shop?col=eden',      image: './edenbanner.jpg' },
   { label: 'The Love Edit',    path: '/shop?col=love-edit', image: './loveEdit.jpg' },
-  { label: 'Bold & Beautiful', path: '/shop?col=bold',      image: './bold.jpg' },
+  { label: 'Bold & Beautiful', path: '/shop?col=bold',      image: './boldbeauty.jpg' },
   { label: 'Featured',         path: '/featured',           image: './featured.jpg' },
   { label: 'All Collections',  path: '/shop',               image: './collection.jpg' },
   { label: 'Sales Promotion',             path: '/sale',               image: './sale.jpg' },
